53bcc4c8ee2318d3b290c4a36b679343eafeaa4e
Babyo7_ <156928258+babyo777@users.noreply.github.com>
2024-05-11 00:15:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ if (cluster.isPrimary) {
   app.get("/", async (req, res) => {
     try {
       const Link = req.query.url;
-      if (Link && StreamAudio.validateID(Link)) {
+      if (Link) {
         if (fs.existsSync(`music/${Link}.mp3`)) {
           const audio = fs.createReadStream(`music/${Link}.mp3`);
           const data = fs.statSync(`music/${Link}.mp3`);
